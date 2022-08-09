776903222de83d10727a005c0184a87daebd1aec
Aute0 <sonya.a.chernyaeva@gmail.com>
2022-08-09 15:38:05 +0300

    ex5 programm to use if/else case

---
+++ ex5_helloHoney/Program.cs
@@ -0,0 +1,9 @@
+﻿// give hello to special user or just hi to another
+Console.WriteLine("Your name is?");
+string username = Console.ReadLine();
+if (username == "Ivan")
+    Console.WriteLine("Hello my sweety, " + username);
+    else
+    {
+        Console.WriteLine("hi, " + username);
+    }
