710a3570eea0e2eb0e8292e701ea5d909e9743f1
Aute0 <sonya.a.chernyaeva@gmail.com>
2022-08-09 15:18:35 +0300

    add changes to program structure

--- ex4_div/Program.cs
+++ ex4_div/Program.cs
@@ -1,6 +1,8 @@
 ﻿// dividing 2 numbers
 // here take int if expect whole result, take double if expect more concreet
 
-double numA = 12;
-double numB = 5;
+double numA = new Random().Next(1, 10); // 1,2,3,...,9
+Console.WriteLine(numA);
+double numB = new Random().Next(1, 10); // 1,2,3,...,9
+Console.WriteLine(numB);
 Console.WriteLine(numA / numB);
