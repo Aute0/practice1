02943c83230de0b9e6f3e81b9244460bcf0352af
Aute0 <sonya.a.chernyaeva@gmail.com>
2022-08-09 22:51:46 +0300

    ex1: find max value from two numbers

--- HW_examples/hw1_ex1/Program.cs
+++ HW_examples/hw1_ex1/Program.cs
@@ -4,21 +4,15 @@
 //a = 2 b = 10->max = 10
 //a = -9 b = -3->max = -3
 
-// Console.WriteLine("Введите число номер 1: ");
-// double num1 = Console.ReadLine();
-// Console.WriteLine("Введите число номер 2: ");
-// double num2 = Console.ReadLine();
+Console.WriteLine("Введите число номер 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число номер 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+int min = 0;
 
-int a=-9;
-int b=-3;
-int max=0;
-int min=0;
-
-if (a > b)
-        {
-            max = a; min = b;}   
-else    {
-            max = b; min = a;}
+if (num1 > num2) max = num1; min = num2;
+if (num1 < num2) max = num2; min = num1;
 
 Console.WriteLine("max= " + max);
 Console.WriteLine("min= " + min);
