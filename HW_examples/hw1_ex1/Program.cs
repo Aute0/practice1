0c26ac35c49407b85fe8e1afd32c5c214abba587
Aute0 <sonya.a.chernyaeva@gmail.com>
2022-08-09 23:35:07 +0300

    change structure of program

--- HW_examples/hw1_ex1/Program.cs
+++ HW_examples/hw1_ex1/Program.cs
@@ -3,16 +3,21 @@
 //a = 5; b = 7->max = 7
 //a = 2 b = 10->max = 10
 //a = -9 b = -3->max = -3
-
+// -----------------
 Console.WriteLine("Введите число номер 1: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число номер 2: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 int max = 0;
-int min = 0;
+// ------------------
+if (num1 >= num2)
+{
+    max = num1;
+    Console.WriteLine("Максимальное число: " + max);
+}
 
-if (num1 > num2) max = num1; min = num2;
-if (num1 < num2) max = num2; min = num1;
-
-Console.WriteLine("max= " + max);
-Console.WriteLine("min= " + min);
+else
+{
+    max = num2;
+    Console.WriteLine("Максимальное число: " + max);
+}
