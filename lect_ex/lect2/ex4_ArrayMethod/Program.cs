fd3e15b70239a70ec34578fc60565878c2bd8d6d
Aute0 <sonya.a.chernyaeva@gmail.com>
2022-08-15 15:51:36 +0300

    prog start array,fill array and print array

---
+++ lect_ex/lect2/ex4_ArrayMethod/Program.cs
@@ -0,0 +1,36 @@
+﻿// Array Method
+
+int [] array = new int[10]; //  по умолчанию массив из 10 нулевых эл-ов
+
+void FillArray(int[] collection) // метод заполнения нулевого массива
+{
+    int length = collection.Length; // длину получим из collection
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10); //заполняем на каждом шаге коллекцию целыми числами от 1 до 10
+        index++;
+
+    }
+}
+
+void PrintArray(int[] collection1) // метод, который делает-не возвращает результат
+{
+
+    int count = collection1.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(collection1[position]);
+        position++;
+    }
+    
+}
+
+FillArray(array);
+PrintArray(array);
+
+
+
+
+
