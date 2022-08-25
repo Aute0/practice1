90b51fc5d9fec5d155a39ad1872e36f88a127629
Aute0 <sonya.a.chernyaeva@gmail.com>
2022-08-25 23:34:38 +0300

    prog calculates sim of even num in array

---
+++ HW_examples/hw5/hw5_ex2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6]-> 0
+
+
+int[] arr = new int[5];
+int k = 0; 
+int sum = 0;
+Random rnd = new Random();
+
+Console.WriteLine("Исходный массив");
+for (int i = 0; i < 5; i++)
+{
+    arr[i] = rnd.Next(-10, 10);
+    Console.Write("{0} ", arr[i]);
+}
+Console.WriteLine();
+
+foreach (int element in arr)
+{
+    Console.Write("{0} ", element);
+    if (element % 2 != 0)
+    {
+        k++;
+        sum += element;
+    }
+}
+Console.WriteLine("\nКоличество нечетных элементов: " + k);
+Console.WriteLine("\nСумма нечетных элементов: " + sum);
+
+
