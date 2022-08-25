f4712d21983ca6fff88a539b61dfd26e4c6d6916
Aute0 <sonya.a.chernyaeva@gmail.com>
2022-08-25 23:10:54 +0300

    file structure staff

---
+++ HW_examples/hw1/hw1_ex1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт,
+// какое число большее, а какое меньшее.
+//a = 5; b = 7->max = 7
+//a = 2 b = 10->max = 10
+//a = -9 b = -3->max = -3
+// -----------------
+Console.WriteLine("Введите число номер 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число номер 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+// ------------------
+if (num1 >= num2)
+{
+    max = num1;
+    Console.WriteLine("Максимальное число: " + max);
+}
+
+else
+{
+    max = num2;
+    Console.WriteLine("Максимальное число: " + max);
+}
---
+++ HW_examples/hw3/hw3_ex1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число
+// и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Твое число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine((num == 0) || (reverse(num, 0) == num));
+Console.ReadKey(true);
+
+int reverse(int num, int mun)
+{
+    while (num > 0) { mun = mun * 10 + num % 10; num /= 10; }
+    return mun;
+}
