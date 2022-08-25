93e45337ef48d7e7f31fbdf9e7540a1ac9810221
Aute0 <sonya.a.chernyaeva@gmail.com>
2022-08-25 23:04:22 +0300

    prog creates array and calculates odd numbers in it

---
+++ HW_examples/hw5_ex1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] arr = new int[5];
+int k = 0;
+Random rnd = new Random();
+
+Console.WriteLine("Исходный массив");
+for (int i = 0; i < 5; i++)
+{
+    arr[i] = rnd.Next(100, 1000);
+    Console.Write("{0} ", arr[i]);
+}
+Console.WriteLine();
+
+foreach (int element in arr)
+{
+    Console.Write("{0} ", element);
+    if (element % 2 == 0)
+    {
+        k++;
+    }
+}
+Console.WriteLine("\nКоличество четных элементов {0}", k);
