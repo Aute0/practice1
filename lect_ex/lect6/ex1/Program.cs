ab75e57e6c93b6c96bafb71f3256feb58ec99d78
Aute0 <sonya.a.chernyaeva@gmail.com>
2022-08-27 00:35:58 +0300

    tt

---
+++ lect_ex/lect6/ex1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Разворот массива
+
+int size = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[size];          // задаем массив с заданием размера от пользователя
+
+for ( int i = 0; i < size; i++)
+{
+    arr[i] = new Random().Next(11); // (0-->10)
+    Console.Write(arr[i] + "\t");   // (\t---> tab)
+}
