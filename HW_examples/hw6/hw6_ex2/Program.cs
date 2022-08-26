06df1f525b7510d3684063a97813ccb1d4bb08c8
Aute0 <sonya.a.chernyaeva@gmail.com>
2022-08-27 00:33:02 +0300

    prog calculates point of intersection of two lines

---
+++ HW_examples/hw6/hw6_ex2/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения
+// двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+// 
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9-> (-0, 5; -0,5)
+
+// FAQ for me:
+// var используем только для double переменных!
+// пересечение прямых считаем по формуле: 
+//              x=-(b1-b2)/(k1-k2);
+//              y=k1*x+b1;
+// Math.Round - Округляет значение до ближайшего целого или указанного
+//              количества десятичных знаков.
+
+
+// ввод данных пользователем:
+
+Console.WriteLine("Insert b1: ");
+var b1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Insert k1: ");
+var k1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Insert b2: ");
+var b2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Insert k2: ");
+var k2 = Convert.ToDouble(Console.ReadLine());
+
+var x = -(b1 - b2) / (k1 - k2);
+var y = k1 * x + b1;
+
+x = Math.Round(x, 2);
+y = Math.Round(y, 2);
+
+Console.WriteLine($"Lines intersect at a point: ({x};{y})");
