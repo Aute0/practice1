7f375800cfcfdb808104f72ebda7be806a70bb85
Aute0 <sonya.a.chernyaeva@gmail.com>
2022-09-05 19:00:45 +0300

    function Akkerman

---
+++ HW_examples/hw9/hw9_ex3/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3->A(m, n) = 9
+
+// ОЧЕНЬ КРАСИВАЯ ФУНКЦИЯ! ^-^
+
+int m = InputNumbers("Введите m: ");
+int n = InputNumbers("Введите n: ");
+
+int AkkermanFunction = Akkerman(m, n);
+
+Console.Write($"Функция Аккермана = {AkkermanFunction} ");
+
+int Akkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0) return Akkerman(m - 1, 1);
+    else return Akkerman(m - 1, Akkerman(m, n - 1));
+}
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
