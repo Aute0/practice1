e6d902e143ec1938f478711aa848044971740d38
Aute0 <sonya.a.chernyaeva@gmail.com>
2022-09-14 14:02:42 +0300

    create program that solve the final task

--- HW_examples/gh_test/Program.cs
+++ HW_examples/gh_test/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача:
+// Написать программу, которая из имеющегося массива строк формирует массив из строк,
+// длина которых меньше либо равна 3 символа. Массив можно ввести с клавиатуры, либо
+// задать на старте алгоритма.
+
+
+string[] ArrayFirst = new string[6] { "2h", "p", "59830", "23df", "res", "sonya"};
+string[] ArrayWork = new string[ArrayFirst.Length];
+void ArraySolution(string[] ArrayFirst, string[] ArrayWork)
+{
+    int count = 0;
+    for (int i = 0; i < ArrayFirst.Length; i++)
+    {
+        if (ArrayFirst[i].Length <= 3)
+        {
+            ArrayWork[count] = ArrayFirst[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+ArraySolution(ArrayFirst, ArrayWork);
+PrintArray(ArrayWork);
